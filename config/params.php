c6644e5039cc3de636f5f6b02bd7bba4fe6ac7d6
Владислав Белан <smakki.92@gmail.com>
2017-02-28 16:07:16 +0600

    register user

--- config/params.php
+++ config/params.php
@@ -2,4 +2,6 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'accessToken' => 'e5eada31d6e5f31abfc08a6a2d39a7d5468f5b15f5357cf8b004dd6fb6a3cbc65945ff8185daef2c014e4',
+    'userID'=>'-129282906',
 ];
